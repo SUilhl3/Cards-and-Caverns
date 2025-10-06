e1d60cf26b67c85b3bda3f4872625ad9104ed641
Tajinder <tnijjar2@my.centennialcollege.ca>
2025-10-06 19:33:27 -0400

    Update RelicManager and create missing scriptable object for one relic

--- Assets/_Project/Scripts/Relics/RelicManager.cs
+++ Assets/_Project/Scripts/Relics/RelicManager.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class RelicManager : MonoBehaviour
 {
-    public List<RelicTemplate> relicList = new List<RelicTemplate>();
+    public List<RelicTemplate> allAvailableRelics = new List<RelicTemplate>(); //list of all available relics
+    public List<RelicTemplate> relicList = new List<RelicTemplate>(); // list of relics player acquired
     public GameObject player;
+
+    //selects a random relic from the list and adds it to the player 
+    //might need to add weights or lower % chance for higher value relics later
+    public void SelectRandomRelic ()
+    {
+        int chosenRelic = Random.Range(0, allAvailableRelics.Count);
+        RelicTemplate relicToAdd = allAvailableRelics[chosenRelic];
+        AddRelic(relicToAdd);
+    }
 
     //adds a relic to the list and calls the onAcquire method
     public void AddRelic(RelicTemplate relic)
