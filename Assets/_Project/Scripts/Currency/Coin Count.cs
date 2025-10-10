806ea437012cbd0bf7d8723ed0b5bd592964d494
Samantha-Schlegel <122414688+Samantha-Schlegel@users.noreply.github.com>
2025-10-10 08:35:10 -0400

    Added Currency

---
+++ Assets/_Project/Scripts/Currency/Coin Count.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+using TMPro; 
+
+public class CoinCount : MonoBehaviour
+{
+    public int count;
+    public TMP_Text CoinCounter; 
+
+    void Start()
+    {
+        count = PlayerPrefs.GetInt("amount", 0); 
+        UpdateCoinDisplay();
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            count += 1;
+            PlayerPrefs.SetInt("amount", count);
+            UpdateCoinDisplay();
+        }
+    }
+
+    void UpdateCoinDisplay()
+    {
+        if (CoinCounter != null)
+        {
+            CoinCounter.text = "Coins: " + count;
+        }
+    }
+}
