bd637dcea1f5c6da9a5503d778c170f06c17d1b6
DaisyBit <DaisyBit@users.noreply.github.com>
2025-11-03 14:41:38 -0500

    Added base for input in options menu, mostly functional pause menu added

---
+++ Assets/UI/Scripts/PauseMenu.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject PauseMenuPanel;
+    public GameObject ConfirmationPanel;
+
+    private const string PauseMenuOpenKey = "PauseMenuOpen";
+    private const string ConfirmationOpenKey = "ConfirmationOpen";
+
+    void Start()
+    {
+        bool wasPauseMenuOpen = PlayerPrefs.GetInt(PauseMenuOpenKey, 0) == 1;
+        bool wasConfirmationOpen = PlayerPrefs.GetInt(ConfirmationOpenKey, 0) == 1;
+
+        PauseMenuPanel.SetActive(wasPauseMenuOpen);
+        ConfirmationPanel.SetActive(wasConfirmationOpen);
+
+        if (wasPauseMenuOpen)
+            Time.timeScale = 0f;
+        else
+            Time.timeScale = 1f;
+    }
+
+    public void ShowPauseMenu()
+    {
+        PauseMenuPanel.SetActive(true);
+        Time.timeScale = 0f;
+        PlayerPrefs.SetInt(PauseMenuOpenKey, 1);
+        PlayerPrefs.Save();
+    }
+
+    public void HidePauseMenu()
+    {
+        PauseMenuPanel.SetActive(false);
+        Time.timeScale = 1f;
+        PlayerPrefs.SetInt(PauseMenuOpenKey, 0);
+        PlayerPrefs.Save();
+    }
+
+    public void ShowConfirmationPanel()
+    {
+        ConfirmationPanel.SetActive(true);
+        PlayerPrefs.SetInt(ConfirmationOpenKey, 1);
+        PlayerPrefs.Save();
+    }
+
+    public void HideConfirmationPanel()
+    {
+        ConfirmationPanel.SetActive(false);
+        PlayerPrefs.SetInt(ConfirmationOpenKey, 0);
+        PlayerPrefs.Save();
+    }
+}
--- Assets/UI/Scripts/SceneSwitcher.cs
+++ Assets/UI/Scripts/SceneSwitcher.cs
@@ -4,16 +4,70 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    private const string LastSceneKey = "LastSceneIndex";
+    private const int OptionsSceneIndex = 3;
+    private const string PauseMenuOpenKey = "PauseMenuOpen";
+    private const string ConfirmationOpenKey = "ConfirmationOpen";
+
     void Start()
     {
-        
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+
+        if (currentSceneIndex != OptionsSceneIndex)
+        {
+            PlayerPrefs.SetInt(LastSceneKey, currentSceneIndex);
+            PlayerPrefs.Save();
+        }
     }
 
-    // Update is called once per frame
-    public void LoadScene(string sceneName)
+    public void LoadOptions()
     {
+        PlayerPrefs.Save();
+        SceneManager.LoadScene(OptionsSceneIndex);
+    }
+
+    public void CloseOptionsAndReturn()
+    {
+        if (PlayerPrefs.HasKey(LastSceneKey))
+        {
+            int lastSceneIndex = PlayerPrefs.GetInt(LastSceneKey);
+
+            PlayerPrefs.SetInt(PauseMenuOpenKey, 1);
+            PlayerPrefs.Save();
+
+            SceneManager.LoadScene(lastSceneIndex);
+        }
+        else
+        {
+            Debug.LogWarning("No last scene found to return to!");
+        }
+    }
+
+    public void LoadTutorial()
+    {
+        ClearPauseState();
         SceneManager.LoadScene(2);
+    }
+
+    public void LoadMap()
+    {
+        ClearPauseState();
+        SceneManager.LoadScene(1);
+    }
+
+    public void LoadMenu()
+    {
+        ClearPauseState();
+        SceneManager.LoadScene(0);
+    }
+
+    public void LoadLastScene()
+    {
+        if (PlayerPrefs.HasKey(LastSceneKey))
+        {
+            int lastSceneIndex = PlayerPrefs.GetInt(LastSceneKey);
+            SceneManager.LoadScene(lastSceneIndex);
+        }
     }
 
     public void QuitGame()
@@ -21,5 +75,10 @@ public class SceneSwitcher : MonoBehaviour
         Application.Quit();
     }
 
-    
+    private void ClearPauseState()
+    {
+        PlayerPrefs.SetInt(PauseMenuOpenKey, 0);
+        PlayerPrefs.SetInt(ConfirmationOpenKey, 0);
+        PlayerPrefs.Save();
+    }
 }
