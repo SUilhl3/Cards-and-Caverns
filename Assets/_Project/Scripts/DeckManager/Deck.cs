8ec0611d040608bc51461c7d4e35831f1bef32ac
SUilhl3 <ilagang@my.centennialcollege.ca>
2025-11-05 01:24:15 -0500

    Manipulate the deck and the discard pile
    - Deck should save now the players card order for both the discard pile and the deck
    - For some reason the buttons don't want to function properly but it should be working properly now.

--- Assets/_Project/Scripts/DeckManager/Deck.cs
+++ Assets/_Project/Scripts/DeckManager/Deck.cs
@@ -6,11 +6,11 @@ public class Deck : MonoBehaviour
 {
     #region Fields and Properties
 
-    public static Deck Instance { get; private set; } 
+    public static Deck Instance { get; private set; }
 
 
     [SerializeField] private CardCollection _playerDeck;
-    [SerializeField] private Card _cardPrefab; 
+    [SerializeField] private Card _cardPrefab;
     [SerializeField] private Canvas _cardCanvas;
     [SerializeField] private bool discardPiledEdited = false;
     [SerializeField] private bool deckPileEdited = false;
@@ -18,12 +18,15 @@ public class Deck : MonoBehaviour
     private List<Card> _deckPile = new();
     private List<Card> _discardPile = new();
     private List<Card> _nextHandCards = new();
-
+    private List<Card> _savedDeckOrder = new();
+    private List<Card> _savedDiscardOrder = new();
     public List<Card> HandCards { get; private set; } = new();
 
 
     private bool isDiscardPileVisible = false;
     private bool isDeckVisible = false;
+    [SerializeField] private GameObject saveDeckOrder;
+    [SerializeField] private GameObject saveDiscardOrder;
 
     #endregion
 
@@ -50,10 +53,10 @@ public class Deck : MonoBehaviour
     {
         for (int i = 0; i < _playerDeck.CardsInCollection.Count; i++)
         {
-            Card card = Instantiate(_cardPrefab, _cardCanvas.transform); 
+            Card card = Instantiate(_cardPrefab, _cardCanvas.transform);
             card.SetUp(_playerDeck.CardsInCollection[i]);
-            _deckPile.Add(card); 
-            card.gameObject.SetActive(false); 
+            _deckPile.Add(card);
+            card.gameObject.SetActive(false);
         }
 
         ShuffleDeck();
@@ -61,35 +64,30 @@ public class Deck : MonoBehaviour
 
     private void ShuffleDeck()
     {
-        for (int i = _deckPile.Count - 1; i > 0; i--) 
+        if (deckPileEdited && _savedDeckOrder.Count == _deckPile.Count)
         {
-            int j = Random.Range(0, i + 1);
-            var temp = _deckPile[i];
-            _deckPile[i] = _deckPile[j];
-            _deckPile[j] = temp;
+            _deckPile = new List<Card>(_savedDeckOrder);
+            deckPileEdited = false;
         }
+
+        else
+        {
+            for (int i = _deckPile.Count - 1; i > 0; i--)
+            {
+                int j = Random.Range(0, i + 1);
+                var temp = _deckPile[i];
+                _deckPile[i] = _deckPile[j];
+                _deckPile[j] = temp;
+            }
+        }
+
     }
 
     public void DrawHand(int amount = 5)
     {
-        //for (int i = 0; i < amount; i++)
-        //{
-        //    if (_deckPile.Count <= 0)
-        //    {
-        //        _discardPile = _deckPile;
-        //        _discardPile.Clear();
-        //        ShuffleDeck();
-        //    }
-        //    if (_deckPile.Count > 0)
-        //    {
-        //        HandCards.Add(_deckPile[0]);
-        //        _deckPile[0].gameObject.SetActive(true);
-        //        _deckPile.RemoveAt(0);
-        //    }
-        //}
 
         Card cardToDraw = null;
-        if(_nextHandCards.Count > 0)
+        if (_nextHandCards.Count > 0)
         {
             cardToDraw = _nextHandCards[0];
             _nextHandCards.RemoveAt(0);
@@ -97,7 +95,7 @@ public class Deck : MonoBehaviour
         }
         else
         {
-            if(_deckPile.Count == 0)
+            if (_deckPile.Count == 0)
             {
                 _deckPile.AddRange(_discardPile);
                 _discardPile.Clear();
@@ -131,35 +129,51 @@ public class Deck : MonoBehaviour
         }
     }
 
-    public void MoveDiscardCard(int oldIndex, int newIndex)
+
+    private void ApplyDiscardOrder()
     {
-        if(oldIndex < 0 || oldIndex >= _discardPile.Count || newIndex < 0 || newIndex >= _discardPile.Count)
+        if (discardPiledEdited && _savedDiscardOrder.Count == _discardPile.Count)
         {
-            return;
+            _discardPile = new List<Card>(_savedDiscardOrder);
         }
-
-        var card = _discardPile[oldIndex];
-        _discardPile.RemoveAt(oldIndex);
-        _discardPile.Insert(newIndex, card);
-        discardPiledEdited = true;
-
+        else
+        {
+            discardPiledEdited = false;
+            _savedDiscardOrder.Clear();
+        }
     }
 
     public void RefillDeckFromDiscardInOrder()
     {
+        ApplyDiscardOrder();
         _deckPile.Clear();
-        _deckPile.AddRange(_discardPile);  // preserves discard pile order
+        _deckPile.AddRange(_discardPile);
         _discardPile.Clear();
+
+        _savedDeckOrder = new List<Card>(_deckPile);
+        deckPileEdited = true;
+        discardPiledEdited = false;
     }
 
-    public void SetNextHandEditedCards(List<Card> eligibleCards)
+    public void SaveDeckOrder(List<Card> orderedCards)
     {
-        _nextHandCards = new List<Card>(eligibleCards);
+        if (orderedCards.Count == _deckPile.Count)
+        {
+            _savedDeckOrder = new List<Card>(orderedCards);
+            deckPileEdited = true;
+        }
+        else
+        {
+            Debug.Log("No cards were saved...");
+        }
     }
+
+
 
     public void DisplayDiscardPile()
     {
-        foreach(Card card in _discardPile)
+        ApplyDiscardOrder();
+        foreach (Card card in _discardPile)
         {
             card.gameObject.SetActive(true);
         }
@@ -172,9 +186,17 @@ public class Deck : MonoBehaviour
             card.gameObject.SetActive(false);
         }
     }
-
+     
     public void DisplayDeckPile()
     {
+        if(discardPiledEdited && _savedDiscardOrder.Count == _deckPile.Count)
+        {
+            _deckPile = new List<Card>(_savedDeckOrder);
+        }
+        else if (deckPileEdited && _savedDeckOrder.Count == _deckPile.Count)
+        {
+            _deckPile = new List<Card>(_savedDeckOrder);
+        }
         foreach (Card card in _deckPile)
         {
             card.gameObject.SetActive(true);
@@ -191,8 +213,16 @@ public class Deck : MonoBehaviour
 
     public void toggleDiscardPile()
     {
-        if (isDiscardPileVisible) {HideDiscardPile();}
-        else {DisplayDiscardPile();}
+        if (isDiscardPileVisible) 
+        { 
+            HideDiscardPile();
+            saveDiscardOrder.SetActive(false);
+        }
+        else
+        {
+            DisplayDiscardPile();
+            saveDiscardOrder.SetActive(true);
+        }
         isDiscardPileVisible = !isDiscardPileVisible;
     }
 
@@ -200,9 +230,35 @@ public class Deck : MonoBehaviour
     {
         Debug.Log($"_deckPile count before display: {_deckPile.Count}");
         Debug.Log($"_discardPile count before display: {_discardPile.Count}");
-        if (isDeckVisible) {HideDeckPile();}
-        else { DisplayDeckPile(); }
+        if (isDeckVisible) 
+        { 
+            HideDeckPile();
+            saveDeckOrder.SetActive(false);
+        }
+        else
+        {
+            if (deckPileEdited && _savedDeckOrder.Count == _deckPile.Count)
+            {
+                _deckPile = new List<Card>(_savedDeckOrder);
+            }
+            DisplayDeckPile();
+            saveDeckOrder.SetActive(true);
+        }
         isDeckVisible = !isDeckVisible;
+    }
+
+    public void SaveDiscardOrderByXPosition()
+    {
+        _savedDiscardOrder = new List<Card>(_discardPile);
+        _savedDiscardOrder.Sort((card1, card2) => card1.transform.position.x.CompareTo(card2.transform.position.x));
+        discardPiledEdited = true;
+    }
+
+    public void SaveDeckOrderByXPosition()
+    {
+        _savedDeckOrder = new List<Card>(_deckPile);
+        _savedDeckOrder.Sort((card1, card2) => card1.transform.position.x.CompareTo(card2.transform.position.x));
+        deckPileEdited = true;
     }
 
     public void DisplayDiscardPileButton()
@@ -212,7 +268,7 @@ public class Deck : MonoBehaviour
 
     public void DisplayDeckButton()
     {
-        ToggleDeckPile();
+        ToggleDeckPile(); 
     }
 
     #endregion
